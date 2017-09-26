7e8f2d7403b89dca9d1d58732e39f2524ab89605
Gallon Lilian <lilian.gallon@etu.u-bordeaux.fr>
2017-09-26 20:27:45 +0200

    Version 0.1: Added licence

--- Bleach/src/me/bleach/commands/Commands.java
+++ Bleach/src/me/bleach/commands/Commands.java
@@ -1,5 +1,23 @@
 package me.bleach.commands;
 
+//This file is part of the Bleach MC AntiCheat.
+//
+//Bleach MC AntiCheat is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//Bleach MC AntiCheat is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with Bleach MC AntiCheat.  If not, see <http://www.gnu.org/licenses/>.
+//Author(s) : Lilian Gallon (N3RO)
+
+
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
